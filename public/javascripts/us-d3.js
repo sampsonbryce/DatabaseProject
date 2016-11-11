4f20135893e45ef164bf0390936b6438d08c23f0
Bryce Sampson <sampson.bryce@yahoo.com>
2016-11-11 05:05:21 +0000

    mostly working need more dynamic

--- public/javascripts/us-d3.js
+++ public/javascripts/us-d3.js
@@ -1,4 +1,4 @@
-function buildMap(coord_array){
+function buildMap(coord_array, color_attribute){
 
   var width = 950,
   height = 550;
@@ -13,41 +13,42 @@ function buildMap(coord_array){
   .projection(projection);
 
 
-  d3.json("/javascripts/us.json", function(error, topo) { console.log(topo);
+  d3.json("/javascripts/us.json", function(error, topo) {
+    console.log(topo);
 
     states = topo.features
 
     // create svg variable
-    var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-    var color = d3.scale.linear().domain([0, 7000]).range([0, 5]);
-
+    var svg = d3.select(".map").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+    var max = d3.max(coord_array, function(d){console.log('scale', d[color_attribute]); return d[color_attribute]})
+    var color = d3.scale.linear().domain([0, max]).range([0, 5]);
+    console.log('MAX:', max);
     // add states from topojson
     svg.selectAll("path")
-    .data(states).enter()
-    .append("path")
-    .attr("class", "feature")
-    .style("fill", "steelblue")
-    .attr("d", path);
+      .data(states).enter()
+      .append("path")
+      .attr("class", "feature")
+      .style("fill", "steelblue")
+      .attr("d", path);
 
     // put boarder around states
     svg.append("path")
-    .datum(topojson.mesh(topo, topo.features, function(a, b) { return a !== b; }))
-    .attr("class", "mesh")
-    .attr("d", path);
-    // console.log("coords", coord_array);
+      .datum(topojson.mesh(topo, topo.features, function(a, b) { return a !== b; }))
+      .attr("class", "mesh")
+      .attr("d", path);
+    console.log("coords", coord_array);
 
     // add circles to svg
     console.log("length:", coord_array.length);
     svg.selectAll("circle")
-    .data(coord_array).enter()
-    .append("circle")
-    .attr("cx", function (d) { console.log("LOGGING", projection(d)); if(projection(d)){return projection(d)[0];} })
-    .attr("cy", function (d) { if(projection(d)){return projection(d)[1];}})
-    .attr("r", "2px")
-    .attr("fill", function(d, i) { /*console.log(d, projection(d));*/ return d3.rgb("red").darker(color(i))} )
+      .data(coord_array).enter()
+      .append("circle")
+      .attr("cx", function (d) { console.log("LOGGING", projection([d.lon, d.lat]), d); if(projection([d.lon, d.lat])){return projection([d.lon, d.lat])[0];} })
+      .attr("cy", function (d) { if(projection([d.lon, d.lat])){return projection([d.lon, d.lat])[1];}})
+      .attr("r", "2px")
+      .attr("fill", function(d, i) { /*console.log(d, projection(d));*/ return d3.rgb("red").darker(color(d.density))} )
 
   });
 
